009ec14b1075ebd3ab0fb8f50a34c69a7c359ce8
Laith Al <31551879+laithal@users.noreply.github.com>
2022-03-18 15:50:22 +0100

    Create Provar-Latest.ps1

---
+++ Provar-Latest.ps1
@@ -0,0 +1,5 @@
+$webclient = New-Object System.Net.WebClient
+$url = "https://download.provartesting.com/latest/Provar_ANT_latest.zip"
+$file = "Provar_ANT_latest.zip"
+$webclient.DownloadFile($url,$file)
+Expand-Archive -LiteralPath Provar_ANT_latest.zip -DestinationPath ProvarHome

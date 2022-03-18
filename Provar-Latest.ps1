8133f4877d90f95bc69af41b61511786229794b4
Laith Al <31551879+laithal@users.noreply.github.com>
2022-03-18 15:55:22 +0100

    Update Provar-Latest.ps1

--- Provar-Latest.ps1
+++ Provar-Latest.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Unrestricted -force
 $webclient = New-Object System.Net.WebClient
 $url = "https://download.provartesting.com/latest/Provar_ANT_latest.zip"
 $file = "Provar_ANT_latest.zip"
